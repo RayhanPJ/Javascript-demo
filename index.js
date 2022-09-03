d2cff461c79e5abbd0208a389052da2e95f4ae9c
RayhanPJ <rayhanputra100@gmail.com>
2022-09-04 03:17:26 +0700

    Nomer 2

--- index.js
+++ index.js
@@ -7,16 +7,24 @@
 // let a = Object.values(obj.num).includes(b) || Object.values(obj.zoo).includes(b) ? b + " adalah " + typeof b : " Data tidak ada";
 // console.log(a);
 
-let i, a, jam, menit, detik;
+// Nomer 1
+// let i, a, jam, menit, detik;
 
-i = 15436;
-a = i % 3600;
+// i = 15436;
+// a = i % 3600;
 
-jam = i / 3600;
+// jam = i / 3600;
 
-menit = a / 60;
+// menit = a / 60;
 
-detik = a % 60;
+// detik = a % 60;
 
-console.log("Konversi jarak dar kota A ke B selama " + i + "s adalah : ");
-console.log(Math.floor(jam) + " Jam ", Math.floor(menit) + " menit ", detik + " detik");
+// console.log("Konversi jarak dar kota A ke B selama " + i + "s adalah : ");
+// console.log(Math.floor(jam) + " Jam ", Math.floor(menit) + " menit ", detik + " detik");
+
+// Nomer 2
+
+// let i = [2, 4, 8, 10, 20, 22, 44];
+// for (let x = 0; x < i.length - 1; x++) {
+//     console.log("Jarak " + i[x] + " ke " + i[x + 1] + " adalah " + (i[x + 1] - i[x]));
+// }
