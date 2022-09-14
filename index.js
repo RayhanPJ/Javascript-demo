fb6a1859f2f4f8a464f23a9852e41517e1fd5ebf
Rayhan Putra Jafandi <rayhanputra100@gmail.com>
2022-09-14 10:25:00 +0700

    penambahan code

--- index.js
+++ index.js
@@ -45,3 +45,50 @@
 // for (let x = 0; x < i.length - 1; x++) {
 //     console.log("Jarak " + i[x], " ke " + i[x + 1], " adalah " + (i[x + 1] - i[x]));
 // }
+
+// let num = [2, 8, 100, 23, 7];
+
+// for (let i = 0; i < num.length; i++) {
+//     let flag = num.length - i - 1;
+//     console.log(flag);
+//     for (let j = 0; j < flag; j++) {
+//         const x = num[j];
+//         const y = num[j + 1];
+//         if (x > y) {
+//             num[j] = y;
+//             num[j + 1] = x;
+//         }
+//         console.log(num);
+//     }
+// }
+
+// LATIHAN SOAL
+
+// Nomer 2
+// function Nilai(nilai) {
+//     switch (true) {
+//         case 91 <= nilai && nilai <= 100:
+//             console.log("A");
+//             break;
+//         case 81 <= nilai && nilai <= 90:
+//             console.log("B");
+//             break;
+//         case 71 <= nilai && nilai <= 80:
+//             console.log("C");
+//             break;
+//         case 61 <= nilai && nilai <= 70:
+//             console.log("D");
+//             break;
+//         case nilai <= 60:
+//             console.log("E");
+//         default:
+//             console.log("HOHOHO");
+//     }
+// }
+// Nilai(85);
+
+let i = 0;
+while (i <= 10) {
+    console.log("Angkot No.", i + " beroperasi dengan baik.");
+    i++;
+}
